d94ae761c48d7de5ca3506ebbf324f1797104352
Your Name <your-email@example.com>
2026-03-08 14:47:07 +0600

    fetch issues from API and display on dashboard complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,15 @@
 const API_URL = "https://phi-lab-server.vercel.app/api/v1/lab"
 let allData = [];
+
+document.getElementById('login-btn').addEventListener('click', () => {
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    if (user === 'admin' && pass === 'admin123') {
+        document.getElementById('login-page').style.setProperty('display', 'none', 'important');
+        document.getElementById('main-page').classList.remove('hidden');
+        fetchData()
+    } else {
+        alert("Invalid Login! Use admin / admin123")
+    }
+});
